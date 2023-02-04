88a52d9ba26d8c64c1967b0a9039cba59a01d7c9
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-02-04 11:14:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,10 @@ import Dashboard from './Dashboard';
 
 
 const App = () => {
-
-//code for  Route path="/" , Route path="/dashboard" 
+  //code for  Route path="/" , Route path="/dashboard" 
 
   const [loggedIn,setLoggedIn]=useState(false)
- const [navigate,setNavigate]=useState(false)
+  const [navigate,setNavigate]=useState(false)
   const[email,setEmail]=useState('')
   const[password,setPassword]=useState('')
   return (
@@ -19,14 +18,15 @@ const App = () => {
 <div id='App'>
   <Router>
     
-    <Switch>
+  <Switch>
       <Route path="/" exact>
-     <Login email={email} password={password} setLoggedIn={setLoggedIn} setNavigate={setNavigate}/>
+        {loggedIn ? <Redirect to="/dashboard"/>: <Login email={email} password={password} setLoggedIn={setLoggedIn} setNavigate={setNavigate}/>}
       </Route>
       
       <Route path="/Register" exact>
         {navigate? <Redirect to="/" /> : <Register  emailChange={setEmail}  passwordChange={setPassword} setNavigate={setNavigate} />}
       </Route>
+
 
       <Route path="/dashboard" exact>
        <Dashboard setLoggedIn={setLoggedIn}/> 
